51176a82644b0f3e34ca1ef8f6fb4a3027bd7f6c
AlesiaBandarenka <happylesechka@gmail.com>
2022-10-11 20:33:16 +0200

    [AB] Add Logo component

---
+++ src/components/Header/Logo/Logo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Logo.scss';
+
+import logoText from '../../../assets/images/logo-text.png';
+import fi_slack from '../../../assets/images/fi_slack.png';
+
+function Logo() {
+	return (
+		<a href='/' className='main-header_logo'>
+			<img
+				className='main-header_logo-image'
+				src={fi_slack}
+				alt='Logo pattern'
+			/>
+			<img className='main-header_logo-image' src={logoText} alt='Logo text' />
+		</a>
+	);
+}
+export default Logo;
