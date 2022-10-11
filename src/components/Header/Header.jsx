406ff8aaac39b9577ea72f17ce685b973738a0cc
AlesiaBandarenka <happylesechka@gmail.com>
2022-10-11 20:32:56 +0200

    [AB] Add toggle-menu for tablets and mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
-import Logo from '../Logo/Logo';
-import NavBar from '../NavBar/NavBar';
-import SearchBar from '../SearchBar/SearchBar';
-import UserBar from '../UserBar/UserBar';
+import Logo from './Logo/Logo';
+import NavBar from './NavBar/NavBar';
+import SearchBar from './SearchBar/SearchBar';
+import UserBar from './UserBar/UserBar';
 
 import './Header.scss';
 
-function Header() {
+export function Header() {
 	return (
 		<header className='main-header'>
+			<h1 class='visually-hidden'>Online beauty store</h1>
+			<button class='main-header__menu-toggle' type='button'>
+				<span class='visually-hidden'>Open menu</span>
+				<span class='main-header__menu-toggle-bar'></span>
+			</button>
 			<Logo />
 			<NavBar />
 			<SearchBar />
@@ -17,4 +22,3 @@ function Header() {
 		</header>
 	);
 }
-export default Header;
