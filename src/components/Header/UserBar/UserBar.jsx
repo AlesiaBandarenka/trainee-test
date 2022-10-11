f16071b639b26960bbf818a8f31e922ecfbb8bf3
AlesiaBandarenka <happylesechka@gmail.com>
2022-10-11 20:34:32 +0200

    [AB] Add UserBar component

---
+++ src/components/Header/UserBar/UserBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './UserBar.scss';
+
+function UserBar() {
+	return (
+		<ul class='user-nav'>
+			<li class='user-nav__item'>
+				<a class='user-nav__link user-nav__link--favorites' href='#'>
+					<span class='visually-hidden'>Favourite</span>
+				</a>
+			</li>
+			<li class='user-nav__item'>
+				<a class='user-nav__link user-nav__link--cart' href='#'>
+					<span class='visually-hidden'>Shopping cart</span>
+				</a>
+			</li>
+		</ul>
+	);
+}
+export default UserBar;
