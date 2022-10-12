f9042a084c45fb5edb711a9329533a7fe99d0174
AlesiaBandarenka <happylesechka@gmail.com>
2022-10-12 19:21:41 +0200

    [AB] Delete unnecessary link

--- src/components/Separator/Separator.jsx
+++ src/components/Separator/Separator.jsx
@@ -3,20 +3,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Separator.scss';
 
-export function Separator({ title, link }) {
+export function Separator({ title }) {
 	return (
 		<div className='main-separator'>
 			<h2 className='main__separator-title'>{title}</h2>
-			<div className='main__separator-wrap'>
-				<a href='#' className='main__separator-link'>
-					{link}
-				</a>
-			</div>
 		</div>
 	);
 }
 
 Separator.propTypes = {
 	title: PropTypes.string,
-	link: PropTypes.string,
 };
