1e8edb7e33f471b54b10fc7af83445590a2be9f2
AlesiaBandarenka <happylesechka@gmail.com>
2022-10-12 19:20:41 +0200

    [AB] Fix bug: replace class gor className

--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -4,26 +4,26 @@ import './NavBar.scss';
 
 function NavBar() {
 	return (
-		<nav class='main-header__nav'>
-			<div class='nav__container'>
-				<ul class='nav__list'>
-					<li class='nav__item'>
-						<a class='nav__link' href='#'>
+		<nav className='main-header__nav'>
+			<div className='nav__container'>
+				<ul className='nav__list'>
+					<li className='nav__item'>
+						<a className='nav__link' href='#'>
 							Shop
 						</a>
 					</li>
-					<li class='nav__item'>
-						<a class='nav__link' href='#'>
+					<li className='nav__item'>
+						<a className='nav__link' href='#'>
 							Products
 						</a>
 					</li>
-					<li class='nav__item'>
-						<a class='nav__link' href='#'>
+					<li className='nav__item'>
+						<a className='nav__link' href='#'>
 							Sale
 						</a>
 					</li>
-					<li class='nav__item'>
-						<a class='nav__link' href='#'>
+					<li className='nav__item'>
+						<a className='nav__link' href='#'>
 							Contact
 						</a>
 					</li>
--- src/components/Header/UserBar/UserBar.jsx
+++ src/components/Header/UserBar/UserBar.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import './UserBar.scss';
 
-function UserBar() {
+function UserBar(productsInCart) {
+	console.log(productsInCart);
 	return (
-		<ul class='user-nav'>
-			<li class='user-nav__item'>
-				<a class='user-nav__link user-nav__link--favorites' href='#'>
-					<span class='visually-hidden'>Favourite</span>
+		<ul className='user-nav'>
+			<li className='user-nav__item'>
+				<a className='user-nav__link user-nav__link--favorites' href='#'>
+					<span className='visually-hidden'>Favourite</span>
 				</a>
 			</li>
-			<li class='user-nav__item'>
-				<a class='user-nav__link user-nav__link--cart' href='#'>
-					<span class='visually-hidden'>Shopping cart</span>
+			<li className='user-nav__item'>
+				{productsInCart && (
+					<a className='user-nav__link user-nav__link--cart-add'></a>
+				)}
+				<a className='user-nav__link user-nav__link--cart' href='#'>
+					<span className='visually-hidden'>Shopping cart</span>
 				</a>
 			</li>
 		</ul>
 	);
 }
 export default UserBar;
+
+UserBar.propTypes = {
+	productsInCart: PropTypes.number,
+};
