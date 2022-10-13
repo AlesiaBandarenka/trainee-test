f051bd7fb5a45b2ad2028fea3f60ff1bc62caa96
AlesiaBandarenka <happylesechka@gmail.com>
2022-10-13 21:24:44 +0200

    [AB] Delete ToggleBar component code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,11 +12,6 @@ export function Header(props) {
 
 	return (
 		<header className='main-header'>
-			<h1 className='visually-hidden'>Online beauty store</h1>
-			<button className='main-header__menu-toggle' type='button'>
-				<span className='visually-hidden'>Open menu</span>
-				<span className='main-header__menu-toggle-bar'></span>
-			</button>
 			<Logo />
 			<NavBar />
 			<SearchBar onChange={onChange} searchTerm={searchTerm} />
