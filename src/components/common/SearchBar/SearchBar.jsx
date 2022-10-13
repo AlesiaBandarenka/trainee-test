fc6ba7f699003898961679d779d13e43d3a6dc49
AlesiaBandarenka <happylesechka@gmail.com>
2022-10-13 22:10:08 +0200

    [AB] Add styles to mobile version

--- src/components/common/SearchBar/SearchBar.jsx
+++ src/components/common/SearchBar/SearchBar.jsx
@@ -6,18 +6,14 @@ function SearchBar(props) {
 	const { onChange, searchTerm } = props;
 
 	return (
-		<>
-			<label htmlFor='search'></label>
-			<input
-				type='text'
-				className='main-header__search'
-				placeholder='Search for products'
-				onChange={onChange}
-				value={searchTerm}
-				// onBlur={onBlur}
-				required
-			/>
-		</>
+		<input
+			type='text'
+			className={`main-header__search ${props.mobile ? 'mobile' : ''}`}
+			placeholder='Search for products'
+			onChange={onChange}
+			value={searchTerm}
+			required
+		/>
 	);
 }
 export default SearchBar;
